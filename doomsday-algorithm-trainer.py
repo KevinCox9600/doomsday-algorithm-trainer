2d05995605ea0f79223477675016303622f68eb9
Kevin Cox <kevindanielcox@gmail.com>
2022-03-14 20:48:47 -0400

    Move code to function

--- doomsday-algorithm-trainer.py
+++ doomsday-algorithm-trainer.py
@@ -10,39 +10,42 @@ r = Style.RESET_ALL
 
 
 def main():
-    i = "init"
     mode = "date"
+    i = "init"
     while i != "q" and i != 0 and i != "":
         if mode == "date":
-            # generate random date
-            start_date = datetime(2000, 1, 1)
-            end_date = datetime(2003, 12, 31)
-            random_date = generate_random_date(start_date, end_date)
-
-            # ask user for date
-            date_string = random_date.strftime("%m/%d/%Y")
-            i = input(
-                f"What day of the week is {date_string}? Number 0-6 (sunday - monday)\n"
-            )
-
-            # switch to config mode if necessary
-            if i == "c":
-                mode = "config"
-                continue
-
-            # validate the date
-            day_of_week = (random_date.weekday() + 1) % 7
-            my_print(day_of_week)
-
-            if str(day_of_week) != i:
-                explain_logic(random_date)
-            else:
-                my_print("That is correct!")
-
+            mode, i = date_mode(mode, i)
             time.sleep(1)
         elif mode == "config":
-            my_print("this is not yet set up")
-            break
+            color_print("this is not yet set up")
+            mode = "date"
+
+
+def date_mode(mode, i):
+    # generate random date
+    start_date = datetime(2000, 1, 1)
+    end_date = datetime(2003, 12, 31)
+    random_date = generate_random_date(start_date, end_date)
+
+    # ask user for date
+    date_string = random_date.strftime("%m/%d/%Y")
+    i = input(f"What day of the week is {date_string}? Number 0-6 (sunday - monday)\n")
+
+    # switch to config mode if necessary
+    if i == "c":
+        mode = "config"
+        return mode, i
+
+    # validate the date
+    day_of_week = (random_date.weekday() + 1) % 7
+    color_print(day_of_week)
+
+    if str(day_of_week) != i:
+        explain_logic(random_date)
+    else:
+        color_print("That is correct!")
+
+    return mode, i
 
 
 def explain_logic(date):
@@ -51,22 +54,24 @@ def explain_logic(date):
     day = date.day
 
     # century doomsday
-    my_print(Y + "CENTURY:")
+    color_print(Y + "CENTURY:")
     century = year // 100
     century_doomsdays = {0: 2, 1: 0, 2: 5, 3: 3}
     century_doomsday = century_doomsdays[century % 4]
-    my_print(f"{century}00's doomsday is {R}{century_doomsday}", True)
+    color_print(f"{century}00's doomsday is {R}{century_doomsday}", True)
 
     # decade doomsday
-    my_print(Y + "DECADE:")
+    color_print(Y + "DECADE:")
     decade = year % 100
     mults_of_12 = decade // 12
     remainder_of_12 = decade % 12
     leap_year_addition = remainder_of_12 // 4
     addition_beyond_mult_of_12 = remainder_of_12 + leap_year_addition
-    my_print(f"The decade can be broken into {mults_of_12 * 12} + {remainder_of_12}")
-    my_print(f"The {mults_of_12 * 12} corresponds to a day index of {B}{mults_of_12}")
-    my_print(
+    color_print(f"The decade can be broken into {mults_of_12 * 12} + {remainder_of_12}")
+    color_print(
+        f"The {mults_of_12 * 12} corresponds to a day index of {B}{mults_of_12}"
+    )
+    color_print(
         f"The {remainder_of_12} corresponds to a day index of {remainder_of_12} + "
         + f"{leap_year_addition} for the leap years, "
         + f"for a result of {R}{addition_beyond_mult_of_12}",
@@ -74,9 +79,9 @@ def explain_logic(date):
     )
 
     # year
-    my_print(Y + "YEAR:")
+    color_print(Y + "YEAR:")
     year_index = century_doomsday + addition_beyond_mult_of_12
-    my_print(
+    color_print(
         f"The century index of {century_doomsday} plus the decade "
         + f"index of {addition_beyond_mult_of_12} "
         + f"results in a year index of {year_index} and a doomsday of {R}{year_index % 7}",
@@ -84,7 +89,7 @@ def explain_logic(date):
     )
 
     # month explanation
-    my_print(Y + "MONTH:")
+    color_print(Y + "MONTH:")
     # add extra day to jan/feb if leap year (year divisible by 4 but not 100)
     leap_year_addition = (
         1 if decade % 4 == 0 and ((decade != 0) or (century % 4 == 0)) else 0
@@ -104,31 +109,31 @@ def explain_logic(date):
         12: 12,
     }
     month_doomsday = month_doomsdays[month]
-    my_print(f"The month is {month}, so the doomsday falls on {month_doomsday}\n")
+    color_print(f"The month is {month}, so the doomsday falls on {month_doomsday}\n")
 
     # day explanation
-    my_print(Y + "DAY:")
+    color_print(Y + "DAY:")
     day_offset = day - month_doomsday
     effective_offset = day_offset % 7
-    my_print(
+    color_print(
         f"The day offset is {B}{day}{r} - {B}{month_doomsday}{r} = {B}{day_offset}{r} -> {R}{effective_offset}",
         True,
     )
 
     # final result
-    my_print(Y + "RESULT:")
+    color_print(Y + "RESULT:")
     result = (year_index + effective_offset) % 7
-    my_print("Adding these together:")
-    my_print(
+    color_print("Adding these together:")
+    color_print(
         f"The year index of {year_index} plus the day offset of {effective_offset}"
     )
-    my_print(f"= {R}{result}", True)
+    color_print(f"= {R}{result}", True)
     weekday = (date.weekday() + 1) % 7
     if result != weekday:
-        my_print(R + "THERE IS AN ERROR" + repr(result) + repr(weekday), True)
+        color_print(R + "THERE IS AN ERROR" + repr(result) + repr(weekday), True)
 
 
-def my_print(text, newline=False):
+def color_print(text, newline=False):
     """Reset print style after printing"""
     print(str(text) + Style.RESET_ALL)
     if newline:
--- todo.md
+++ todo.md
@@ -7,3 +7,4 @@ Future Changes:
 - [ ] Make it clear how to exit program (and don't print explanation if exiting)
 - [ ] Add history of specific incorrect guesses and what proportion correct
 - [ ] Add recording of what type of mistakes I made and what frequency they occurred
+- [ ] Add timer
